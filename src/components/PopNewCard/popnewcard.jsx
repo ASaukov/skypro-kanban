bcbecb973d9f72bfe486208b458ca24e4f4ed78f
ASaukov <Saukov72@mail.ru>
2024-07-12 02:06:53 +0500

    new card styled

--- src/components/PopNewCard/popnewcard.jsx
+++ src/components/PopNewCard/popnewcard.jsx
@@ -1,3 +1,4 @@
+
 import { Calendar } from "../Calendar/calendar";
 import * as S from "./popnewcard.styled"
 
@@ -8,59 +9,56 @@ export const PopNewCard = () => {
         <S.PopNewCardBlock>
           <S.PopNewCardContent>
             <S.PopNewCardTtl>Создание задачи</S.PopNewCardTtl>
-            <a href="#" className="pop-new-card__close">
+            <S.PopNewCardClose href="#">
               &#10006;
-            </a>
-            <div className="pop-new-card__wrap">
-              <form
-                className="pop-new-card__form form-new"
+            </S.PopNewCardClose>
+            <S.PopNewCardWrap>
+              <S.PopNewCardForm
                 id="formNewCard"
                 action="#"
               >
-                <div className="form-new__block">
-                  <label htmlFor="formTitle" className="subttl">
+                <S.FormNewBlock>
+                  <S.Subttl htmlFor="formTitle">
                     Название задачи
-                  </label>
-                  <input
-                    className="form-new__input"
+                  </S.Subttl>
+                  <S.FormNewInput
                     type="text"
                     name="name"
                     id="formTitle"
                     placeholder="Введите название задачи..."
                     autoFocus
                   />
-                </div>
-                <div className="form-new__block">
-                  <label htmlFor="textArea" className="subttl">
+                </S.FormNewBlock>
+                <S.FormNewBlock>
+                  <S.Subttl htmlFor="textArea">
                     Описание задачи
-                  </label>
-                  <textarea
-                    className="form-new__area"
+                  </S.Subttl>
+                  <S.FormNewArea
                     name="text"
                     id="textArea"
                     placeholder="Введите описание задачи..."
-                  ></textarea>
-                </div>
-              </form>
+                  ></S.FormNewArea>
+                </S.FormNewBlock>
+              </S.PopNewCardForm>
               <Calendar />
-            </div>
-            <div className="pop-new-card__categories categories">
-              <p className="categories__p subttl">Категория</p>
+            </S.PopNewCardWrap>
+            <S.PopNewCardCategories>
+              <S.CategoriesP>Категория</S.CategoriesP>
               <div className="categories__themes">
-                <div className="categories__theme _orange _active-category">
-                  <p className="_orange">Web Design</p>
-                </div>
-                <div className="categories__theme _green">
-                  <p className="_green">Research</p>
-                </div>
-                <div className="categories__theme _purple">
-                  <p className="_purple">Copywriting</p>
-                </div>
+                <S.CategoriesOrangeActive>
+                  <S.OrangeCat>Web Design</S.OrangeCat>
+                </S.CategoriesOrangeActive>
+                <S.CategoriesGreen>
+                  <S.GreenCat>Research</S.GreenCat>
+                </S.CategoriesGreen>
+                <S.CategoriesPurple>
+                  <S.PurpleCat>Copywriting</S.PurpleCat>
+                </S.CategoriesPurple>
               </div>
-            </div>
-            <button className="form-new__create _hover01" id="btnCreate">
+            </S.PopNewCardCategories>
+            <S.FormNewCreate id="btnCreate">
               Создать задачу
-            </button>
+            </S.FormNewCreate>
           </S.PopNewCardContent>
         </S.PopNewCardBlock>
       </S.PopNewCardContainer>
--- src/components/PopNewCard/popnewcard.styled.js
+++ src/components/PopNewCard/popnewcard.styled.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Green, Hover01, Orange, Purple } from "../../globalStyle.styled";
 
 export const PopNewCard = styled.div`
   /* display: none; */
@@ -46,4 +47,178 @@ export const PopNewCardTtl = styled.h3`
   font-weight: 600;
   line-height: 24px;
   margin-bottom: 20px;
+`
+
+export const PopNewCardClose = styled.a`
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  color: #94A6BE;
+  cursor: pointer;
+
+  &:hover {
+    color: #000000;
+  }
+`
+
+export const PopNewCardWrap = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`
+
+export const PopNewCardForm = styled.form`
+  max-width: 370px;
+  width: 100%;
+  display: block;
+  margin-bottom: 20px;
+`
+
+export const FormNewBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Subttl = styled.label`
+  color: #000;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+`
+
+export const FormNewInput = styled.input`
+  width: 100%;
+  outline: none;
+  padding: 14px;
+  background: transparent;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  border-radius: 8px;
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.14px;
+  margin: 20px 0;
+
+  &::-moz-placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1px;
+    color: #94A6BE;
+    letter-spacing: -0.14px;
+  }
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1px;
+    color: #94A6BE;
+    letter-spacing: -0.14px;
+  }
+`
+
+export const FormNewArea = styled.textarea`
+  width: 100%;
+  outline: none;
+  padding: 14px;
+  background: transparent;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  border-radius: 8px;
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: -0.14px;
+  max-width: 370px;
+  margin-top: 14px;
+  height: 200px;
+
+  &::-moz-placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1px;
+    color: #94A6BE;
+    letter-spacing: -0.14px;
+}
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1px;
+    color: #94A6BE;
+    letter-spacing: -0.14px;
+}
+`
+
+export const FormNewCreate = styled.button`
+  width: 132px;
+  height: 30px;
+  background-color: #565EEF;
+  border-radius: 4px;
+  border: 0;
+  outline: none;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1;
+  color: #FFFFFF;
+  float: right;
+
+  ${Hover01}
+`
+
+export const PopNewCardCategories = styled.div`
+  position: static;
+  width: 570px;
+  height: 63px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0px;
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+  margin-bottom: 20px;
+`
+
+export const CategoriesP = styled.p`
+  margin-bottom: 14px;
+  color: #000;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+`
+
+const CategoriesTheme = css`
+  display: inline-block;
+  width: auto;
+  height: 30px;
+  padding: 8px 20px;
+  border-radius: 24px;
+  margin-right: 7px;
+  opacity: 0.4;
+`
+
+export const CategoriesOrangeActive = styled.div`
+  ${CategoriesTheme};
+  ${Orange};
+  opacity: 1 !important;
+`
+
+export const CategoriesGreen = styled.div`
+  ${CategoriesTheme};
+  ${Green};
+`
+
+export const CategoriesPurple = styled.div`
+  ${CategoriesTheme};
+  ${Purple};
+`
+
+export const OrangeCat = styled.p`
+${Orange}
+`
+
+export const GreenCat = styled.p`
+${Green}
+`
+
+export const PurpleCat = styled.p`
+${Purple}
 `
