8d0e091d4cce6b418361f5c3359b4e4203c5f709
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-07-20 01:22:17 +0500

    taskContext

---
+++ src/components/Calendar/calendar.styled.js
@@ -0,0 +1,6 @@
+import { DayPicker } from "react-day-picker";
+import styled from "styled-components";
+
+export const StyledDayPicker = styled(DayPicker)`
+--rdp-cell-size: 40px;
+`
--- src/context/TaskContext/TaskContext.jsx
+++ src/context/TaskContext/TaskContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const TaskContext = createContext(null);
+
+export const TaskProvider = ({children}) => {
+    const [tasks, setTasks] = useState([])
+
+    return (
+        <TaskContext.Provider value={{tasks, setTasks}}>
+            {children}
+        </TaskContext.Provider>
+    )
+}
